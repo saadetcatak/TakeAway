256acdda134b53c141f58a04aafa226bb6bd3d10
Saadet Çatak Akgüneş <saadetcatak1453@gmail.com>
2024-07-18 20:46:05 +0300

    update

---
+++ Services/Order/Infrastructure/TakeAway.Persistence/Context/OrderContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TakeAway.Domain.Entities;
+
+namespace TakeAway.Persistence.Context
+{
+    public class OrderContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=SAADET\\SQLEXPRESS01; initial catalog=TakeAwayOrderDb; integrated security=true");
+        }
+
+        public DbSet<Address> Address { get; set; }
+        public DbSet<OrderDetail> OrderDetail { get; set; }
+        public DbSet<Ordering> Ordering { get; set; }
+    }
+}
